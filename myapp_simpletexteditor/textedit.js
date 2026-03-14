f3a205209e7e26bcb0cfb588867f42a0b94111d4
amdemw206-coder <amdemw206@gmail.com>
2026-03-13 21:56:53 -0700

    For Assignment

---
+++ myapp_simpletexteditor/textedit.js
@@ -0,0 +1,48 @@
+
+class TextOperation {
+  constructor(type, char) {
+    this.type = type; 
+    this.char = char; 
+  }
+}; 
+
+class TextEditor{
+    constructor(){
+        this.text = ""
+        this.history = [];
+    }
+    addCharacter(charToAdd){
+        this.text += charToAdd;
+        const addChar = new TextOperation('add', charToAdd)
+        this.history.push(addChar); 
+        console.log(`Added: "${charToAdd}" | Current Text: "${this.text}"`);
+    }
+
+    deleteCharacter(){
+        const charToDelete = this.text.slice(-1)
+        this.text = this.text.slice(0,-1)
+        const delChar = new TextOperation('delete', charToDelete)
+        this.history.push(delChar); 
+        console.log(`Deleted: "${charToDelete}" | Current Text: "${this.text}"`);
+
+    }
+    undoCharacter(){
+        const charToUndo = this.history.pop()
+        if (charToUndo.type === 'add'){
+          this.text = this.text.slice(0,-1)
+          console.log(`Undo-Add: Removed "${charToUndo.char}"`);  
+        }else if (charToUndo.type == 'delete'){
+            this.text += charToUndo.char
+        console.log(`Undo-Delete: Added "${charToUndo.char}"`)    
+        }
+    }
+    
+}; 
+
+const textedit = new TextEditor(); 
+
+textedit.addCharacter("H")
+textedit.addCharacter("i")
+textedit.deleteCharacter()
+textedit.undoCharacter()
+console.log(textedit.text)
